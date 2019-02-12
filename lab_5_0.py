bf52a66260aafae79f49079e5f1b09a950f057b9
Tai Dunn <39572984+taidunn@users.noreply.github.com>
2019-02-12 12:23:56 -0500

    Create lab_5_0.py

---
+++ lab_5_0.py
@@ -0,0 +1,52 @@
+#Tai Dunn
+#CSCI 1200A
+#Lab Exercise 4.1
+#02/11/2019
+#lab_5_0.py
+
+# Task 1
+print('------------------------------------------------------')
+print('Task 1')
+
+p= 42
+q = 7
+
+# Task 2
+print('------------------------------------------------------')
+print('Task 2')
+
+print(str(p) + ' == ' + str(q) + ' ? ' + str(p == q))
+
+# Task 3
+print('------------------------------------------------------')
+print('Task 3')
+
+print(str(p) + ' != ' + str(q) + ' ? ' + str(p != q))
+
+# Task 4
+print('------------------------------------------------------')
+print('Task 4')
+
+print(str(p) + ' < ' + str(q) + ' ? ' + str(p < q))
+print(str(p) + ' > ' + str(q) + ' ? ' + str(p > q))
+print(str(p) + ' <= ' + str(q) + ' ? ' + str(p <= q))
+print(str(p) + ' >= ' + str(q) + ' ? ' + str(p >= q))
+
+# Task 5
+print('------------------------------------------------------')
+print('Task 5')
+
+print('p < q and q < p ? ' + str((p < q) and str(q < p)))
+print('p < q and q > p ? ' + str((p < q) and str(q > p)))
+print('p > q and q > p ? ' + str((p > q) and str(q > p)))
+print('p > q and q < p ? ' + str((p > q) and str(q < p)))
+
+
+# Task 6
+print('------------------------------------------------------')
+print('Task 6')
+
+print('p < q or q > p ? ' + str((p < q) or str(q > p)))
+print('p < q or q < p ? ' + str((p < q) or str(q < p)))
+print('p > q or q > p ? ' + str((p > q) or str(q > p)))
+print('p > q or q < p ? ' + str((p > q) or str(q < p)))
