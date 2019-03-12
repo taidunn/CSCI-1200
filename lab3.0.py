da871631b5cbbcdc23aa0a54c6c3fb1fc6247172
Tai Dunn <39572984+taidunn@users.noreply.github.com>
2019-03-12 11:46:23 -0400

    Create lab3.0.py

---
+++ lab3.0.py
@@ -0,0 +1,32 @@
+#Tai Dunn
+#Lab 3.0
+#CSCI 1200
+
+#Empty List
+Big_Bang_Theory = []
+print(Big_Bang_Theory)
+
+Big_Bang_Theory.append('Sheldon')
+print(Big_Bang_Theory)
+
+Big_Bang_Theory.append('Leonard')
+print(Big_Bang_Theory)
+
+Big_Bang_Theory.append('Howard')
+print(Big_Bang_Theory)
+
+Big_Bang_Theory.append('Rajesh')
+print(Big_Bang_Theory)
+
+Big_Bang_Theory.insert(2, 'Penny')
+print(Big_Bang_Theory)
+
+Big_Bang_Theory.insert(5, 'Bernadette')
+print(Big_Bang_Theory)
+
+Big_Bang_Theory.append('Emily')
+print(Big_Bang_Theory)
+
+Big_Bang_Theory.remove('Emily')
+Big_Bang_Theory.append('Stuart')
+print(Big_Bang_Theory)
