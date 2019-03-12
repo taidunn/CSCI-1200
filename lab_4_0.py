64bb5dcdd2b0c76d8a3e3a0f3c7ebd74519e052f
Tai Dunn <39572984+taidunn@users.noreply.github.com>
2019-03-12 11:45:26 -0400

    Create lab_4_0.py

---
+++ lab_4_0.py
@@ -0,0 +1,29 @@
+#Tai Dunn
+#CSCI 1200A
+#Lab Exercise 4.0
+#02/04/2019
+#lab_4_0.py
+
+print('------------------------------------------------------')
+print('Task 1')
+friends = ['sounisa', 'xavier', 'anastasia', 'chloe', 'marianne']
+
+print('------------------------------------------------------')
+print('Task 2')
+print(len(friends))
+
+print('------------------------------------------------------')
+print('Task 3')
+friends.sort()
+print(friends)
+
+print('------------------------------------------------------')
+print('Task 4')
+for jajas in friends:
+	print("You're a great friend, " + jajas.title() + '!')\
+
+print('------------------------------------------------------')
+print('Task 5')	
+for jajas in friends:
+	print("You're a great friend, " + jajas.title() + '!')
+print("Thanks for being such great friends")
